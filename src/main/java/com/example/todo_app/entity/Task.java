1dd3906a93958e61592b438f9395d69d061fc817
Lakshay Singla <lakshay02singla@gmail.com>
2025-01-19 12:02:35 +0530

    New fields added

--- src/main/java/com/example/todo_app/entity/Task.java
+++ src/main/java/com/example/todo_app/entity/Task.java
@@ -1,8 +1,13 @@
 package com.example.todo_app.entity;
 
+import com.example.todo_app.constants.Priority;
 import com.example.todo_app.constants.TaskStatus;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -11,8 +16,20 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long taskId;
+
     private String taskDescription;
 
     @Enumerated(EnumType.STRING)
     private TaskStatus taskStatus;
+
+    @Enumerated(EnumType.STRING)
+    private Priority taskPriority;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
